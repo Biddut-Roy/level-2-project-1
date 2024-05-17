99f924ce1667939c06cc904507f713f3a3041826
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-05-17 12:33:58 +0600

    old json vertion module.p file under and new vertion mjs eslin problem solve

--- src/App.ts
+++ src/App.ts
@@ -1,10 +1,13 @@
-import express, { Application, Request, Response } from 'express'
-const app: Application = express()
+import express, { Application, Request, Response } from 'express';
+const app: Application = express();
+import cors from 'cors';
 
-app.use(express.json())
+// parser
+app.use(express.json());
+app.use(cors());
 
-app.get('/', (req: Request, res:Response) => {
-  res.send('Hello World!')
-})
+app.get('/', (req: Request, res: Response) => {
+  res.send(res);
+});
 
 export default app;
